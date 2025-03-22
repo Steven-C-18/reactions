0d88415823178733219e5210c687335454ebd136
Steve <steveniclark@outlook.com>
2025-03-22 15:47:09 +0000

    test2

---
+++ migrations/v1/v100_reactions.php
@@ -0,0 +1,25 @@
+<?php
+/**
+	* Reactions $extends the phpBB Software package.
+	* @copyright (c) 2024, Steve, https://steven-clark.tech/
+	* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+ 
+//namespace steve\reactions\migrations\v1;
+
+class v100_reactions extends \phpbb\db\migration\migration
+{
+	public function effectively_installed()
+	{
+		return isset($this->config['reactions_enabled']);
+	}
+	
+	static public function depends_on()
+	{
+		//return ['\phpbb\db\migration\data\v320\v320'];
+	}
+
+	public function update_data()
+	{	
+	}
+}
